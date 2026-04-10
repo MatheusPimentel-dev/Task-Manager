f31b92e04cfdb9bf863402b1e6c0d749e231f482
Matheus Pimentel <matheuspimentel.pimentel@gmail.com>
2026-04-10 13:28:26 -0300

    Atualinzando data de tarefas recorrentes

--- app/scheduler/jobs.py
+++ app/scheduler/jobs.py
@@ -12,6 +12,7 @@ def verificar_tarefas_pendentes():
     for t in task:
         if horarioAtual.hour == 0 and horarioAtual.minute == 0:
             resetar_notificacoes(t)
+            atualizar_agendamento(t)
 
         elif t['scheduled_time'] == now and not t['done'] and not t['notification_sent']:
             enviar_notificacao(t)
@@ -21,6 +22,31 @@ def resetar_notificacoes(task):
     task['notification_sent'] = False
     TaskService.update_task(task)
 
+def atualizar_agendamento(task):
+
+    if not task['fixed'] or task['days_of_week'] is None:
+        return
+    
+    hoje = datetime.now()
+    proxima_data = datetime.strptime(task['scheduled_time'], "%Y-%m-%d %H:%M")
+
+    if(proxima_data >= hoje):
+        return
+    
+    dias_semana = task['days_of_week'].split(',')
+    dias_semana = [int(dia) for dia in dias_semana]
+
+    i = 0
+    while i < 7:
+        proxima_data += timedelta(days=1)
+
+        if proxima_data.weekday() in dias_semana:
+            break
+
+        i += 1
+    
+    task['scheduled_time'] = proxima_data.strftime("%Y-%m-%d %H:%M")
+    TaskService.update_task(task)
 
 def enviar_notificacao(task):
     message = f"📣 Tarefa: {task['title']} - Hora de realizar a tarefa! Duração: {task['duration']} minutos."
